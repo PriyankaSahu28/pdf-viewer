2e8d6a61e361cb23c25df06b17514bc890e39a3b
Priyanka Sahu <priyankasahu1428@gmail.com>
2022-12-28 03:30:43 +0530

    modify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import PdfViewerComponent from "./components/PdfViewerComponent";
+import Entry from './components/Entry';
+//import  {Doc1} from './components/Entry';
+// import  Doc2 from './components/doc1.pdf';
+// import  Doc3 from './components/doc1.pdf';
+
+import "./App.css";
+
+
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch, Routes,Route,Link
+} from "react-router-dom";
+
 
 function App() {
+  
+
   return (
+    
+   
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Router>
+      <Routes>
+      <Route path="/" element={<Entry />}></Route>
+      
+
+
+
+       {/* <Switch>  */}
+      
+        {/* <Route path="/doc1" element={<Doc1 />}></Route> */}
+        {/* <Route path="/doc2" element={<Doc2 />}></Route>
+         <Route path="/doc3" element={<Doc3/>}></Route>  */}
+         {/* </Switch>  */}
+         </Routes>
+         </Router>
     </div>
+    
+    
   );
 }
 
---
+++ src/components/Entry.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useState } from "react";
+import PdfViewerComponent from "./PdfViewerComponent";
+
+ import './Entry.css';
+import {Link} from "react-router-dom";
+
+const Entry = () => {
+    const [document, setDocument] = useState("");
+  
+  return (
+    <div>
+       
+      <h1>Documents </h1>
+      <h1 className='line'>________________</h1>
+     
+      <p className='doc1'><a href="javascript:void(0);" onClick={() => setDocument("doc1.pdf")}>- Sample document1.pdf</a>
+      </p>  
+   
+      
+        <p className='doc2'><a href="javascript:void(0);"  onClick={() => setDocument("doc2.pdf")}>- Sample document2.pdf</a></p>
+      
+          <p className='doc3'><a href="javascript:void(0);" onClick={() => setDocument("doc3.pdf")}>- Sample document3.pdf</a></p>
+    
+          <div className="App-viewer">
+        <PdfViewerComponent document={document} />
+      </div>
+    </div>
+    
+  )
+ 
+}
+// export function Doc1(){
+//     const [document, setDocument] = useState("");
+//     return
+//       <p className='doc1'><a href="javascript:void(0);" onClick={() => setDocument("doc1.pdf")}>- Sample document1.pdf</a>
+//       </p> 
+  
+//     }
+//     export function Doc2(){
+//         const [document, setDocument] = useState("");
+//         return
+//           <p className='doc2'><a href="javascript:void(0);" onClick={() => setDocument("doc2.pdf")}>- Sample document1.pdf</a>
+//           </p> 
+      
+//         }
+//         export function Doc3(){
+//             const [document, setDocument] = useState("");
+//             return
+//               <p className='doc3'><a href="javascript:void(0);" onClick={() => setDocument("doc3.pdf")}>- Sample document1.pdf</a>
+//               </p> 
+          
+//             }
+
+export default Entry
